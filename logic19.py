2fa2ca5dba4aaa92fb110d4a638d8182a61c757d
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-11 09:52:34 +0500

    19

--- logic19.py
+++ logic19.py
@@ -11,5 +11,5 @@ def main(x):
     s1=x%10
     s2=x%10//10
     s3=x//10
-    return 9<x and x<1000 and (s1*100+s2*10+s3*1==x or s1%10==s3//10)
-print(main(232))    
+    return 9<x and x<1000 and (s1*100+s2*10+s3*1==x or x%10==x//10)
+print(main(22))    
