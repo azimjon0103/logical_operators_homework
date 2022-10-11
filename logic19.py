df20c80b86ec264b70b2224e15f749974e7bbd94
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-11 10:28:52 +0500

    19

--- logic19.py
+++ logic19.py
@@ -9,7 +9,8 @@ def main(x):
         bool: answer
     """
     s1=x%10
-    s2=x%10//10
-    s3=x//10
-    return 9<x and x<1000 and (s1*100+s2*10+s3*1==x or x%10==x//10)
-print(main(22))    
+    s2=x%100//10
+    s3=x//100
+    print(s1*100+s2*10+s3*1)
+    return (9<x and x<1000) and (s1*100+s2*10+s3*1==x or x%10==x//10) and type(x)==type(1)
+print(main(232))    
